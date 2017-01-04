793a0c1508dd2c8e8e4bd22267ac92c4913c0161
Jave Fang <xinghong.fang@gmail.com>
2017-01-04 01:25:14 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup file for aqara package."""
+from setuptools import setup
+
+setup(name='pyaqara',
+      version='0.2.0',
+      description='Python API for talking to an Aqara gateway',
+      url='https://github.com/javefang/pyaqara',
+      author='Xinghong Fang',
+      license='MIT',
+      packages=['aqara'],
+      zip_safe=True)
