7a4bcbb6323cc9c3d30ff3bcd3edf9030b4fa719
Jave Fang <xinghong.fang@gmail.com>
2017-01-13 00:32:37 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 """Setup file for aqara package."""
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='pyaqara',
-      version='0.2.0',
-      description='Python API for talking to an Aqara gateway',
-      url='https://github.com/javefang/pyaqara',
-      author='Xinghong Fang',
-      license='MIT',
-      packages=['aqara'],
-      zip_safe=True)
+  version='0.2.0',
+  description='Python API for interfacing with the Aqara gateway',
+  url='https://github.com/javefang/pyaqara',
+  download_url='https://github.com/javefang/pyaqara/tarball/0.2.0',
+  author='Xinghong Fang',
+  author_email= 'xinghong.fang@gmail.com',
+  license='MIT',
+  packages=['aqara'],
+  keywords = ['aqara', 'home', 'automation', 'sensor'],
+  zip_safe=True,
+)
