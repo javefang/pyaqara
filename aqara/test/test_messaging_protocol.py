4817ad104ee5d71b8fe4d0cb3b83155b4c303605
Jave Fang <xinghong.fang@gmail.com>
2017-01-22 16:42:51 +0000

    updated filenames

--- aqara/test/test_messaging_protocol.py
+++ aqara/test/test_messaging_protocol.py
@@ -3,32 +3,32 @@ import json
 from asyncio import DatagramTransport
 from unittest.mock import MagicMock
 
-from .. import client
+from .. import messaging_protocol
 from ..const import (MCAST_ADDR, MCAST_PORT, GATEWAY_PORT)
 
 def test_handle_message():
     """test_handle_message"""
-    mock_client = client.AqaraMessagingProtocol()
-    mock_client.handle_message = MagicMock()
+    mock_protocol = messaging_protocol.AqaraMessagingProtocol()
+    mock_protocol.handle_message = MagicMock()
     test_data = {"model": "test"}
     test_addr = "10.10.10.10"
 
     # run test
-    mock_client.datagram_received(test_data, test_addr)
+    mock_protocol.datagram_received(test_data, test_addr)
 
     # assert
-    mock_client.handle_message.assert_called_with(test_data, test_addr)
+    mock_protocol.handle_message.assert_called_with(test_data, test_addr)
 
 def test_unicast():
     """test_unicast"""
     mock_transport = DatagramTransport()
     mock_transport.sendto = MagicMock()
-    mock_client = client.AqaraMessagingProtocol()
-    mock_client.transport = mock_transport
+    mock_protocol = messaging_protocol.AqaraMessagingProtocol()
+    mock_protocol.transport = mock_transport
     test_data = {"cmd": "read"}
     test_addr = "10.10.10.10"
 
-    mock_client.unicast(test_addr, test_data)
+    mock_protocol.unicast(test_addr, test_data)
 
     test_data_encoded = json.dumps(test_data).encode('utf-8')
     mock_transport.sendto.assert_called_with(test_data_encoded, (test_addr, GATEWAY_PORT))
@@ -37,11 +37,11 @@ def test_broadcast():
     """test_broadcast"""
     mock_transport = DatagramTransport()
     mock_transport.sendto = MagicMock()
-    mock_client = client.AqaraMessagingProtocol()
-    mock_client.transport = mock_transport
+    mock_protocol = messaging_protocol.AqaraMessagingProtocol()
+    mock_protocol.transport = mock_transport
     test_data = {"cmd": "read"}
 
-    mock_client.broadcast(test_data)
+    mock_protocol.broadcast(test_data)
 
     test_data_encoded = json.dumps(test_data).encode('utf-8')
     mock_transport.sendto.assert_called_with(test_data_encoded, (MCAST_ADDR, MCAST_PORT))
