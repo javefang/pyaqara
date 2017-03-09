5f39426f18773d2fd82e4c834f55e915c1167ae2
Jave Fang <xinghong.fang@gmail.com>
2017-03-09 01:52:34 +0000

    bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='pyaqara',
-  version='0.3.0',
+  version='0.4.0',
   description='Python API for interfacing with the Aqara gateway',
   url='https://github.com/javefang/pyaqara',
   download_url='https://github.com/javefang/pyaqara/tarball/0.2.0',
