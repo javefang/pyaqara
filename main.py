878df92571c7b9a033ec8002f00c959029bdab3b
jave <xinghong.fang@gmail.com>
2017-03-13 20:29:31 +0000

    check if env var is set before continue

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 import logging
 import asyncio
 import os
+import sys
 from aqara.client import AqaraClient
 
 _LOGGER = logging.getLogger(__name__)
 
+def check_var(key):
+    if key not in os.environ:
+        _LOGGER.error("Environment variable '%s' not set", key)
+        sys.exit(1)
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
+
+    check_var("AQARA_GW_SID")
+    check_var("AQARA_GW_SECRET")
     gw_sid = os.environ["AQARA_GW_SID"]
     gw_secret = os.environ["AQARA_GW_SECRET"]
+
     aqara_client = AqaraClient({gw_sid: gw_secret})
     loop.run_until_complete(aqara_client.start(loop))
     try:
